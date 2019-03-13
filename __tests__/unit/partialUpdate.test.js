ef246c81a6c7f5cfb365d5a51038d20aab7dd8cd
Ethan Shen <ethanshen.wu@gmail.com>
2019-03-13 10:16:31 -0700

    2nd test for partialUpdate

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -10,13 +10,31 @@ describe("partialUpdate()", () => {
         let table = 'companies';
         let items = {description: 'test description'};
         let key = 'handle';
-        let id = 'Test1';
+        let id = 'TEST';
 
         let partialUpdateResult = partialUpdate(table, items, key, id);
     // FIXME: write real tests!
     expect(partialUpdateResult).toEqual({ query:
-      'UPDATE companies SET description=$1 WHERE handle=$2 RETURNING *',
-     values: [ 'test description', 'Test1' ] });
+      `UPDATE companies SET description=$1 WHERE handle=$2 RETURNING *`,
+     values: [ 'test description', 'TEST' ] });
+
+  });
+
+  it("should generate a proper update query with many fields inserted",
+      function () {
+        let table = 'companies';
+        let items = {description: 'test description',
+                     name: 'Test1',
+                     num_employees: 100,
+                     logo_url: 'http://test.com'};
+        let key = 'handle';
+        let id = 'TEST';
+
+        let partialUpdateResult = partialUpdate(table, items, key, id);
+    // FIXME: write real tests!
+    expect(partialUpdateResult).toEqual({ query:
+      `UPDATE companies SET description=$1, name=$2, num_employees=$3, logo_url=$4 WHERE handle=$5 RETURNING *`,
+     values: [ 'test description', 'Test1', 100, 'http://test.com', 'TEST' ] });
 
   });
 });
