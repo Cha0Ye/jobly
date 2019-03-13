7a17265d3d59221065d1373f7dde68130a41db9e
Chao Ye <Chao12.ye@gmail.com>
2019-03-13 14:55:22 -0700

    all routes added for companies

--- models/company.js
+++ models/company.js
@@ -33,8 +33,44 @@ class Company {
             params)
 
         return companiesResult.rows
+    }
+    
+    static async addCompany({handle, name, num_employees, description, logo_url}){
+        const result = await db.query(
+            `INSERT INTO companies (handle, name, num_employees, description, logo_url) 
+             VALUES ($1, $2, $3, $4, $5)
+            RETURNING handle, name, num_employees, description, logo_url`, 
+            [handle, name, num_employees, description, logo_url]);
+            return result.rows[0];
+    }
 
-    } 
+    static async patchCompany({query, values}){
+
+        const update = await db.query(query, values);
+        return update.rows[0];
+    }
+
+
+    static async getOneCompany(handle){
+        
+        let company = await db.query(
+            `SELECT handle, name, num_employees, description, logo_url
+             FROM companies
+             WHERE handle=$1`,[handle]
+        );
+        return company.rows[0];
+    }
+
+
+    static async deleteCompany(handle){
+        let deletedCompany = await db.query(
+            `DELETE from companies 
+            WHERE handle =$1
+            RETURNING handle, name`,[handle]
+        );
+        return deletedCompany.rows[0];
+    
+    }
 }
 
 module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ExpressError = require('../helpers/expressError');
 const Company = require('../models/company')
-
+const sqlForPartialUpdate = require('../helpers/partialUpdate');
 
 const jsonschema = require('jsonschema');
 //add schemas
@@ -15,6 +15,66 @@ router.get("/", async function(req, res, next) {
         let {search, min_employees, max_employees} = req.query;
         let companies = await Company.searchByQuery({search, min_employees, max_employees});
         return res.json({companies}); 
+    }
+    catch(err){
+        next(err);
+    }
+});
+
+
+router.get('/:handle', async function(req, res, next){
+    try{
+        debugger
+        let handle = req.params.handle;
+        let company = await Company.getOneCompany(handle);
+        return res.json({company});
+    }
+    catch(err){
+        next(err);
+    }
+});
+
+
+
+router.post("/", async function(req, res, next) {
+    try{
+        const {handle, name, num_employees, description, logo_url} = req.body;
+        let company = await Company.addCompany({handle, name, num_employees, description, logo_url});
+        return res.status(201).json({company});
+    }
+    catch(err){
+        return next(err);
+    }
+});
+
+
+router.patch("/:handle", async function(req, res, next){
+    try{
+        
+        let table = 'companies';
+        let items = req.body;
+        let key = 'handle';
+        let id = req.params.handle;
+        let partialUpdateQuery = sqlForPartialUpdate(table, items, key, id);
+        let company = await Company.patchCompany(partialUpdateQuery);
+        return res.json({company});
+        
+    }
+    catch(err){
+        next(err);
+    }
+
+});
+
+router.delete('/:handle', async function(req, res, next){
+    try{
+        debugger
+        let handle = req.params.handle;
+        let deletedCompany = await Company.deleteCompany(handle);
+        if(deletedCompany === undefined){
+            throw new ExpressError('No Such Company!',404);
+        }
+        return res.json({"message":"Company deleted"});
     }
     catch(err){
         next(err);
