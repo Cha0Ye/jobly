3660b5295c4b791b42b580b7fd0abdbd9f7bea71
Chao Ye <Chao12.ye@gmail.com>
2019-03-13 10:06:54 -0700

    partialUpdate Test1 ran

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,22 @@
+process.env.NODE_ENV = 'test';
+const  partialUpdate = require('../../helpers/partialUpdate');
+
+
+
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
+        let table = 'companies';
+        let items = {description: 'test description'};
+        let key = 'handle';
+        let id = 'Test1';
 
+        let partialUpdateResult = partialUpdate(table, items, key, id);
     // FIXME: write real tests!
-    expect(false).toEqual(true);
+    expect(partialUpdateResult).toEqual({ query:
+      'UPDATE companies SET description=$1 WHERE handle=$2 RETURNING *',
+     values: [ 'test description', 'Test1' ] });
 
   });
 });
